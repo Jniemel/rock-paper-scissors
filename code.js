d312f568472463d59724498b3c09d62f51f1a095
Jniemel <juho_n@live.fi>
2023-03-30 20:16:40 +0300

    Finished game

--- code.js
+++ code.js
@@ -10,27 +10,49 @@ function playRound(playerSelection, computerSelection) {
    let pSelected = playerSelection.toLowerCase();
    
    if (pSelected == "rock" && computerSelection == "rock") {
-    return "Draw! Rock against rock!";
+    return 0;
    } else if ( pSelected == "rock" && computerSelection == "scissors") {
-    return "You win! Rock beats scissors!";
+    return 1;
    } else if ( pSelected == "rock" && computerSelection == "paper") {
-    return "You lose! Rock loses against paper!";
+    return -1;
    } else if ( pSelected == "paper" && computerSelection == "rock") {
-    return "You win! Paper beats rock!";
+    return 1;
    } else if ( pSelected == "paper" && computerSelection == "paper") {
-    return "Draw! Paper agaisnt paper!";
+    return 0
    } else if ( pSelected == "paper" && computerSelection == "scissors") {
-    return "You lose! Paper loses to scissors!";
+    return -1;
    } else if ( pSelected == "scissors" && computerSelection == "rock") {
-    return "You lose! Scissors lose to rock!";
+    return -1;
    } else if ( pSelected == "scissors" && computerSelection == "scissors") {
-    return "Draw! Scissors against scissors!";
+    return 0;
    } else if ( pSelected == "scissors" && computerSelection == "paper") {
-    return "You win! Scissors beat paper!";
+    return 1;
    }
 
 }
 
 function game() {
-    
+    let points = 0;
+    let result = 0;
+    let selection = "";    
+    for (let i = 0; i < 5; i++) {
+        selection = prompt("Rock, paper or scissors?");
+        result = playRound(selection, getComputerChoice())
+        if (result == -1) {
+            points = points - 1;
+            console.log("You lost! -1 point!");
+        } else if (result == 0) {
+            console.log("Draw! 0 points!")
+        } else if (result == 1) {
+            points = points + 1;
+            console.log("You win! +1 point!")
+        }        
+    }
+    if (points > 0) {
+        console.log("You won the game!")
+    } else if (points == 0) {
+        console.log("The game was draw!")
+    } else if ( points < 0) {
+        console.log("You lost the game!")
+    }
 }
